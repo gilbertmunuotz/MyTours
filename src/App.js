0d1f22765028951cb598117288bc5f7804079699
gilbertmunuotz <officialgilbert24@gmail.com>
2024-01-28 21:24:54 -0800

    Simple Tests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Header from "./Components/Header";
+
 function App() {
   return (
     <div className="App">
       <div className="bg-red-500">hello world</div>
+      <Header/>
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Header() {
     return (
         <div className='Header'>
 
-
+            <div className="bg-red-500">hello world</div>
 
 
         </div>
