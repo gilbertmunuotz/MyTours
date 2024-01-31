97d0afad750a994133eb30ec38eaa5be239d77c6
gilbertmunuotz <officialgilbert24@gmail.com>
2024-01-30 21:14:28 -0800

    ....

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,32 +1,43 @@
 import React from 'react'
+import ReactPlayer from 'react-player/lazy'
+// import myVideo from "../Videos/Welcome";
 
 function Body() {
     return (
         <div className='Body'>
-            <h1 className='text-5xl text-center my-8 uppercase'>John Deere Adventures</h1>
-            <p className='mx-14 first-letter:text-5xl first-letter:font-bold'>Welcome to John Deere Adventures!
+            <h1 className='text-5xl text-center mt-10 uppercase'>John Deere Adventures</h1>
+            <p className='mx-14 text-xl first-letter:text-5xl'>Welcome to John Deere Adventures!
 
                 Since 2000, John Deere Adventures has been leading unforgettable tours in the heart of East Africa, showcasing the breathtaking beauty and unique cultures of Tanzania. We're passionate about sharing our love for this incredible country with travelers from all over the world, and we offer a wide range of tours to suit every interest and budget.
 
-                Our Roots in Arusha
-
                 Our journey began in the vibrant city of Arusha, Tanzania's safari capital. Nestled at the foot of Mount Meru, Arusha is the perfect gateway to the Serengeti National Park, the Ngorongoro Crater, and other world-renowned wildlife reserves.
 
-                Unforgettable Experiences Await
+                Unforgettable Experiences Await</p>
 
-                Whether you're dreaming of spotting lions and zebras on a classic safari, trekking through the lush rainforests of Kilimanjaro, or immersing yourself in the rich culture of the Maasai people, John Deere Adventures has the perfect tour for you. We offer a variety of itineraries, from short day trips to multi-day safaris, and we can tailor our tours to meet your specific needs and interests.
 
-                More Than Just Safaris
+            <div className="whychoose">
+                <div className="grid grid-cols-2 gap-3 mx-14 mt-8">
+                    <div className="">
+                        <ReactPlayer
+                            className=""
 
-                While safaris are a highlight of any trip to Tanzania, we believe there's so much more to this incredible country than just wildlife. We offer cultural tours that take you to meet the Maasai people and learn about their fascinating traditions, as well as culinary adventures that introduce you to the delicious flavors of Tanzanian cuisine.
 
-                Committed to Sustainable Tourism
+                        />
+                    </div>
 
-                We are committed to responsible tourism practices and believe in protecting the environment and supporting local communities. We work with local guides and businesses, and we offset the carbon footprint of our tours.
 
-                Ready to embark on your own Tanzanian adventure?
+                    <div className="mx-12">
+                        <h2 className='text-2xl text-center font-bold'>Why Choose Us</h2>
+                        <p className='text-xl'>We are committed to responsible tourism practices and believe in protecting the environment and supporting local communities. We work with local guides and businesses, and we offset the carbon footprint of our tours.
 
-                Contact John Deere Adventures today and start planning your unforgettable journey!</p>
+                            Ready to embark on your own Tanzanian adventure?
+                            
+                            Contact John Deere Adventures today and start planning your unforgettable journey!</p>
+                    </div>
+
+
+                </div>
+            </div>
         </div>
     )
 }
